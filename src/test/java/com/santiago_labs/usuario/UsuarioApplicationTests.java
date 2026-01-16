ef1ffd647c2e343ff583f24f3d6f068dca17f66a
Mauricio Santiago <santiagodev6@hotmail.com>
2026-01-15 22:05:12 -0300

    commit inicial

---
+++ src/test/java/com/santiago_labs/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago_labs.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
