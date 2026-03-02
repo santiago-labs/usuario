e3c63094350b0727e56ad0e7844c24970945001f
Mauricio Santiago <santiagodev6@hotmail.com>
2026-03-02 20:59:59 -0300

    adicionados metodos de gravar endereço e telefone do usuario

--- src/main/java/com/santiago_labs/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/santiago_labs/usuario/business/converter/UsuarioConverter.java
@@ -110,4 +110,23 @@ public class UsuarioConverter {
                 .numero(dto.getNumero()!= null ? dto.getNumero(): entity.getNumero())
                 .build();
     }
+    public  Endereco paraEnderecoEntity (EnderecoDTO dto, Long idUsuario){
+        return Endereco.builder()
+                .rua(dto.getRua())
+                .numero(dto.getNumero())
+                .complemento(dto.getComplemento())
+                .cidade(dto.getCidade())
+                .cep(dto.getCep())
+                .estado(dto.getEstado())
+                .usuario_id(idUsuario)
+                .build();
+    }
+    public Telefone paraTelefoneEntity(TelefoneDTO dto, Long idUsuario){
+        return Telefone.builder()
+
+                .numero(dto.getNumero())
+                .ddd(dto.getDdd())
+                .usuario_id(idUsuario)
+                .build();
+    }
 }
